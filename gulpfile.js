a8d372d41699bcbbc20834c95eccfd8e07a38a73
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-10-21 11:57:22 -0700

    adding gulpfile and index.html

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+// Assigning modules to local variables
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Rahul Raviprasad - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2015-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' */\n',
+    ''
+].join('');
+
+// Default task
+gulp.task('default', ['minify-css', 'minify-js', 'copy']);
+
+// Minify CSS
+gulp.task('minify-css', function() {
+    return gulp.src('css/main.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('js/main.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy Bootstrap core files from node_modules to vendor directory
+gulp.task('bootstrap', function() {
+    return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'))
+})
+
+// Copy jQuery core files from node_modules to vendor directory
+gulp.task('jquery', function() {
+    return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'))
+})
+
+// Copy Font Awesome core files from node_modules to vendor directory
+gulp.task('fontawesome', function() {
+    return gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('vendor/font-awesome'))
+})
+
+// Copy all third party dependencies from node_modules to vendor directory
+gulp.task('copy', ['bootstrap', 'jquery', 'fontawesome']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Watch Task that watches for HTML or JS changes and reloads with browserSync
+gulp.task('dev', ['browserSync', 'minify-css', 'minify-js'], function() {
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
